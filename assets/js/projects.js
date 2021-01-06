f710dc83199adacd873859c8fe63035f06463543
Dennis kiarie <dennizwarui@gmail.com>
2021-01-07 00:19:56 +0300

    add scroll function for project cards

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -12,6 +12,28 @@ function getProjects(){
     })
 }
 
+function bind_listeners_for_scroll(){
+    let projects_slider = $('.project_slider'),
+        btn_slide_left = $('#slide_left'),
+        btn_slide_right = $('#slide_right');
+
+    btn_slide_left.click(function (){
+        console.log(projects_slider.width());
+        projects_slider.animate({
+            scrollLeft : "-="+(projects_slider.width()/1.5),
+        }, 300)
+    });
+
+    btn_slide_right.click(function (){
+        projects_slider.animate({
+            scrollLeft : "+="+(projects_slider.width()/1.5),
+        }, 300)
+    });
+
+
+}
+
 $(document).ready(function () {
-   getProjects();
+   // getProjects();
+    bind_listeners_for_scroll()
 });
